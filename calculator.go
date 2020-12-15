d9bc6d204000a0b9215f04fa458e25ee8d25133e
sdoyle88 <r3tr0pi3@gmail.com>
2020-12-15 07:20:02 -0800

    Working through Go Book

--- calculator.go
+++ calculator.go
@@ -3,11 +3,17 @@ package calculator
 
 // Add takes two numbers and returns the result of adding them together.
 func Add(a, b float64) float64 {
-return a + b
+	return a + b
 }
 
 // Subtract takes two numbers and returns the result of subtracting the second
 // from the first.
 func Subtract(a, b float64) float64 {
 	return b - a
+}
+
+// Multiply takes two numbers and returns the result of mulitipling the second
+// from the first.
+func Multiply(a, b float64) float64 {
+	return a * b
 }
